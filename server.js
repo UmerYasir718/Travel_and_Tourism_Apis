ca41c49fa50ddbebc55322ae4dba3a9bc0bbcd1e
UmerYasir718 <you@example.com>
2024-01-16 01:35:42 +0500

    umer

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(
   })
 );
 const countriesRecord = require("./Models/CountryRecord");
-app.get("/admin/:countryName", async (req, res) => {
+app.get("/find/:countryName", async (req, res) => {
   try {
     const { countryName } = req.params;
     console.log(countryName);
