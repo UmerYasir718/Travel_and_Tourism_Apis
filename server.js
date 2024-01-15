2b12d77b2bcc63562cafd0486d4ae4be6169ba8e
UmerYasir718 <you@example.com>
2024-01-16 01:21:40 +0500

    Umer

--- server.js
+++ server.js
@@ -14,25 +14,25 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
-// const countriesRecord = require("./Models/CountryRecord");
-// app.get("/home/:countryName", async (req, res) => {
-//   try {
-//     const { countryName } = req.params;
-//     console.log(countryName);
+const countriesRecord = require("./Models/CountryRecord");
+app.get("/home/:countryName", async (req, res) => {
+  try {
+    const { countryName } = req.params;
+    console.log(countryName);
 
-//     // Use the countryName parameter to filter data in the MongoDB collection
-//     const data = await countriesRecord.findOne({ country: countryName });
-//     if (!data) {
-//       res.status(404).json({ error: "Country not found" });
-//       return;
-//     }
+    // Use the countryName parameter to filter data in the MongoDB collection
+    const data = await countriesRecord.findOne({ country: countryName });
+    if (!data) {
+      res.status(404).json({ error: "Country not found" });
+      return;
+    }
 
-//     res.json({ data });
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 const UserCountry = require("./Routes/Users/UserCountry");
 const UserRoutes = require("./Routes/Users/UserRoutes");
