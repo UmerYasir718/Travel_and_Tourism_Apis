cccc81ffd7ba946fdb7d7c414f69c40baa3b5115
UmerYasir718 <you@example.com>
2024-01-21 15:46:57 +0500

    Changes

--- Routes/Users/UserContact.js
+++ Routes/Users/UserContact.js
@@ -8,8 +8,6 @@ router.use(cors());
 router.post("/contact", async (req, res) => {
   try {
     const { firstName, lastName, email, subject, issue } = req.body;
-    console.log(req.body);
-    console.log(firstName, lastName, email, subject, issue);
     // Simple validation, ensure all fields are provided
     if (!firstName || !lastName || !email || !subject || !issue) {
       return res.status(400).json({ error: "Please Fill all required fields" });
